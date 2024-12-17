96e16cb77ac7a37d440643c9cd4ef197e07b209a
facuduran31 <facundoduran31@gmail.com>
2024-12-17 13:25:58 -0300

    Bootstrap settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/pages/login/login.component';
 import { MainComponent } from './components/pages/main/main.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 const routes: Routes = [
-  {path: '', component: AppComponent}
+  {path: '', component: LoginComponent}
 ];
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
