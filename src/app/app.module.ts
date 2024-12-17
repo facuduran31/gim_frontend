61ef43bbce8a0dd5830554d9da1481596f632cbd
facuduran31 <facundoduran31@gmail.com>
2024-12-17 16:08:43 -0300

    sidebar, navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,20 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/pages/login/login.component';
 import { MainComponent } from './components/pages/main/main.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SidebarComponent } from './components/utilities/sidebar/sidebar.component';
+import { NavbarComponent } from './components/utilities/navbar/navbar.component';
 
 const routes: Routes = [
-  {path: '', component: LoginComponent}
+  {path: '', component: LoginComponent},
+  {path: 'main', component: MainComponent}
 ];
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    MainComponent
+    MainComponent,
+    SidebarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
