a99aca18d62bde8ef93718325793b12aeec6c476
facuduran31 <facundoduran31@gmail.com>
2024-12-18 10:42:59 -0300

    ajustes

--- src/app/components/pages/main/main.component.ts
+++ src/app/components/pages/main/main.component.ts
@@ -8,10 +8,11 @@ import { Component } from '@angular/core';
 export class MainComponent {
 
   options = [
-    {title: 'Gestionar mis gimnasios', subtitle: 'Mis gimnasios', classes: 'fas fa-dumbbell', color: 'primary'},
+    {title: 'Gestiona tus gimnasios', subtitle: 'Mis gimnasios', classes: 'fas fa-dumbbell', color: 'primary'},
     {title: 'Gestiona tu suscripcion', subtitle: 'Mi suscripcion', classes: 'fas fa-dollar-sign', color: 'success'},
     {title: 'Gestiona tu cuenta', subtitle: 'Mi cuenta', classes: 'fas fa-user', color: 'danger'},
     {title: 'Soporte', subtitle: 'Necesito ayuda', classes: 'fas fa-circle-question', color: 'warning'}
   ]
 
+  
 }
