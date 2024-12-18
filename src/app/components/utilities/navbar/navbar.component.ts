7a49f94701021bae81856209e55aea2ba989bb88
facuduran31 <facundoduran31@gmail.com>
2024-12-18 15:15:50 -0300

    logout

--- src/app/components/utilities/navbar/navbar.component.ts
+++ src/app/components/utilities/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { AuthService } from 'src/app/services/authservice.service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,5 +12,12 @@ export class NavbarComponent {
   notificaciones = [
     {redirectTo: '#', mensaje: 'Notificacion 1', fecha: '17 de Diciembre de 2024'}
   ];
+
+  constructor(private authService: AuthService, private route: Router) { }
+
+  logout() {
+    this.authService.logout();
+    this.route.navigate(['/']);
+  }
 
 }
