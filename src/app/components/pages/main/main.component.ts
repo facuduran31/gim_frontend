bedbccdd89a0be8bb5744663d7e6ade54ae15057
facuduran31 <facundoduran31@gmail.com>
2024-12-18 10:47:09 -0300

    tablas y graficos

--- src/app/components/pages/main/main.component.ts
+++ src/app/components/pages/main/main.component.ts
@@ -11,7 +11,9 @@ export class MainComponent {
     {title: 'Gestiona tus gimnasios', subtitle: 'Mis gimnasios', classes: 'fas fa-dumbbell', color: 'primary'},
     {title: 'Gestiona tu suscripcion', subtitle: 'Mi suscripcion', classes: 'fas fa-dollar-sign', color: 'success'},
     {title: 'Gestiona tu cuenta', subtitle: 'Mi cuenta', classes: 'fas fa-user', color: 'danger'},
-    {title: 'Soporte', subtitle: 'Necesito ayuda', classes: 'fas fa-circle-question', color: 'warning'}
+    {title: 'Soporte', subtitle: 'Necesito ayuda', classes: 'fas fa-circle-question', color: 'warning'},
+    {title: 'Ver gráficos', subtitle: 'Ver gráficos', classes: 'fas fa-chart-pie', color: 'info'},
+    {title: 'Ver tablas', subtitle: 'Ver tablas', classes: 'fas fa-table', color: 'dark'}
   ]
 
   
