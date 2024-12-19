cd15725e9dbaf1f68b00c856e1b2752c898c60e7
facuduran31 <facundoduran31@gmail.com>
2024-12-19 14:58:02 -0300

    tarjetas de gimnasio dinamicas

--- src/app/components/pages/mis-gimnasios/mis-gimnasios.component.ts
+++ src/app/components/pages/mis-gimnasios/mis-gimnasios.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Gimnasio } from 'src/app/models/gimnasio';
+import { AuthService } from 'src/app/services/authservice.service';
+import { GimnasioService } from 'src/app/services/gimnasio.service';
 
 @Component({
   selector: 'app-mis-gimnasios',
@@ -7,4 +10,17 @@ import { Component } from '@angular/core';
 })
 export class MisGimnasiosComponent {
 
+  constructor(private authService: AuthService, private gimnasioService: GimnasioService) { this.obtenerGimnasios() }
+
+  usuarioLogeado = this.authService.getUsuario();
+  gimnasios: Gimnasio[] = [];
+
+  obtenerGimnasios() {
+    if (this.usuarioLogeado.id) {
+      this.gimnasioService.getGimnasiosByIdUsuario(this.usuarioLogeado.id).subscribe(gimnasio => 
+        {
+          this.gimnasios = [...gimnasio];
+        });
+    }
+  }
 }
