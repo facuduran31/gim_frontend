8f65c82d7a848ab4dc957244dc9928ac53f82b2e
facuduran31 <facundoduran31@gmail.com>
2024-12-19 16:09:48 -0300

    crear gimnasio

--- src/app/components/utilities/form-gimnasio/form-gimnasio.component.ts
+++ src/app/components/utilities/form-gimnasio/form-gimnasio.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Gimnasio } from 'src/app/models/gimnasio';
+import { Usuario } from 'src/app/models/usuario';
+import { AuthService } from 'src/app/services/authservice.service';
+import { GimnasioService } from 'src/app/services/gimnasio.service';
 
 @Component({
   selector: 'app-form-gimnasio',
@@ -6,5 +11,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-gimnasio.component.css']
 })
 export class FormGimnasioComponent {
+
+  constructor(private gimnasioService: GimnasioService, private authService: AuthService, private route: Router) { }
+
+  gimnasio: Gimnasio = new Gimnasio('', '');
+  usuarioLogeado: Usuario = this.authService.getUsuario();
+  inputNombreGimnasio: string = '';
+  inputLogoGimnasio: string = '';
+
+  crearGimnasio() {
+    this.gimnasio.nombre = this.inputNombreGimnasio;
+    this.gimnasio.logo = this.inputLogoGimnasio;
+    this.gimnasio.idUsuario = this.usuarioLogeado.id;
+    this.gimnasioService.createGimnasio(this.gimnasio).subscribe(
+      (response) => {
+        alert('Gimnasio creado');
+        this.route.navigate(['/mis-gimnasios']);
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 
 }
--- src/app/models/gimnasio.ts
+++ src/app/models/gimnasio.ts
@@ -1,5 +1,11 @@
-export interface Gimnasio {
-    id: number;
+export class Gimnasio {
+    id: number | undefined;
     nombre: string;
     logo: string;
+    idUsuario: number | undefined;
+
+    constructor(nombre: string, logo: string) {
+        this.nombre = nombre;
+        this.logo = logo;
+    }
 }
