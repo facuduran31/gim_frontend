94496b826a86535d8452daa2059b82e441105341
facuduran31 <facundoduran31@gmail.com>
2025-02-09 15:47:58 -0300

    tabla planes

--- src/app/components/pages/planes/planes.component.ts
+++ src/app/components/pages/planes/planes.component.ts
@@ -16,15 +16,9 @@ export class PlanesComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
-      console.log(params);
-      
       const idGimnasio = parseInt(params.get('id') || '0')
       this.planesService.getPlanesByIdGimnasio(idGimnasio).subscribe(planes => {
         this.planes = [...planes];
-        console.log(this.planes);
-        console.log(idGimnasio);
-        
-        
       });
     });
   }
