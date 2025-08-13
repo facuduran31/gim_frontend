419620764716ae8cfd9ac6c279e6767e78706282
facuduran31 <facundoduran31@gmail.com>
2025-08-13 13:38:23 -0300

    accion borrar plan

--- src/app/components/pages/planes/planes.component.ts
+++ src/app/components/pages/planes/planes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Route, Router } from '@angular/router';
 import { Plan } from 'src/app/models/plan';
 import { PlanService } from 'src/app/services/plan.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-planes',
@@ -13,7 +14,7 @@ export class PlanesComponent implements OnInit {
   modoCrearPlan: boolean = false;
   planes: Array<Plan> = [];
 
-  constructor(private planesService: PlanService, private route:ActivatedRoute) { }
+  constructor(private planesService: PlanService, private route:ActivatedRoute, private router:Router) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
@@ -30,4 +31,34 @@ export class PlanesComponent implements OnInit {
   toggleModoCrearPlan() {
     this.modoCrearPlan = !this.modoCrearPlan;
   }
+
+  borrarPlan(id: number) {
+      Swal.fire({
+        title: '¡ADVERTENCIA!',
+        text: 'SI BORRAS EL PLAN TODA LA INFORMACIÓN SE PERDERÁ DE MANERA DEFINITIVA',
+        icon: 'warning',
+        showConfirmButton: true,
+        showCloseButton: true,
+        showDenyButton: true,
+        confirmButtonText: 'BORRAR',
+        denyButtonText: 'Cancelar',
+        confirmButtonColor: '#ff0000',
+        denyButtonColor: '#555555'
+      }).then((result) => {
+        if(result.isConfirmed)
+        {
+          this.planesService.deletePlan(id).subscribe(() => {
+            Swal.fire({
+              title: 'Plan borrado con exito',
+              icon: 'success',
+              confirmButtonText: 'Ok',
+              confirmButtonColor: '#00aa00',
+            }).then((result) => {
+              this.ngOnInit();
+            })
+          });
+        }
+        
+      })
+    }
 }
