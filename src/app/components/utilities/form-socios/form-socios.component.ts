2b9e5c464fe26d93a265d973df353d8923f3d110
Pedro <pedrogarrido971@gmail.com>
2025-09-19 17:27:57 -0300

    Termine crear socio con inscripcion

--- src/app/components/utilities/form-socios/form-socios.component.ts
+++ src/app/components/utilities/form-socios/form-socios.component.ts
@@ -5,6 +5,9 @@ import { SociosService } from 'src/app/services/socios.service';
 import Swal from 'sweetalert2';
 import { Plan } from 'src/app/models/plan';
 import { PlanService } from 'src/app/services/plan.service';
+import { Inscripcion } from 'src/app/models/inscripcion';
+import { InscripcionesService } from 'src/app/services/inscripciones.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-form-socios',
@@ -14,36 +17,39 @@ import { PlanService } from 'src/app/services/plan.service';
 export class FormSociosComponent {
 
   modoEditar: boolean = false;
-  
+
 
   @Input() socio: Socio = new Socio(0, "", "", "", "", false, 0);
 
-  planes:Array<Plan>=[new Plan(0,'Seleccione un plan','',0,0,0,0)];
+  planes: Array<Plan> = [new Plan(0, 'Seleccione un plan', '', 0, 0, 0, 0)];
+
+  inscripcion: Inscripcion = new Inscripcion(null, 0, 0, new Date(), new Date(), '', '');
 
 
 
-  constructor(private sociosService: SociosService, private route: ActivatedRoute, private planService: PlanService) { }
+  constructor(private sociosService: SociosService, private route: ActivatedRoute, private planService: PlanService, private inscripcionesService: InscripcionesService) { }
 
   inputDniSocio: string = "";
   inputNombreSocio: string = "";
   inputApellidoSocio: string = "";
   inputTelefonoSocio: string = "";
   inputEstadoSocio: boolean = false;
-  inputPlan:Plan= this.planes[0];
+  inputPlan: Plan = this.planes[0];
 
 
   ngOnInit() {
-    this.route.paramMap.subscribe(params =>{
-      this.socio.idSocio=parseInt(params.get('idSocio') || '0');
-      if(this.socio.idSocio>0){
-        this.modoEditar=true;
-        this.sociosService.getSocioById(this.socio.idSocio).subscribe((res:any)=>{
-          this.socio.nombre=res.nombre;
-          this.socio.apellido=res.apellido;
-          this.socio.dni=res.dni;
-          this.socio.estado=res.activo;
-          this.socio.idGimnasio=res.idGimnasio;
-          this.socio.telefono=res.telefono;
+    this.route.paramMap.subscribe(params => {
+      this.socio.idGimnasio = parseInt(params.get('id') || '0');
+      this.socio.idSocio = parseInt(params.get('idSocio') || '0');
+      if (this.socio.idSocio > 0) {
+        this.modoEditar = true;
+        this.sociosService.getSocioById(this.socio.idSocio).subscribe((res: any) => {
+          this.socio.nombre = res.nombre;
+          this.socio.apellido = res.apellido;
+          this.socio.dni = res.dni;
+          this.socio.estado = res.activo;
+          this.socio.idGimnasio = res.idGimnasio;
+          this.socio.telefono = res.telefono;
           this.inputDniSocio = this.socio.dni;
           this.inputNombreSocio = this.socio.nombre;
           this.inputApellidoSocio = this.socio.apellido;
@@ -52,8 +58,8 @@ export class FormSociosComponent {
         })
       }
     });
-    this.planService.getPlans().subscribe((res:any)=>{
-      res.forEach((plan:any)=>{
+    this.planService.getPlans().subscribe((res: any) => {
+      res.forEach((plan: any) => {
         this.planes.push(new Plan(plan.idPlan, plan.nombre, plan.descripcion, plan.precio, plan.duracion, plan.diasPorSemana, plan.idGimnasio))
       })
     })
@@ -67,31 +73,47 @@ export class FormSociosComponent {
   }
 
 
+
   crearSocio() {
     this.socio.dni = this.inputDniSocio;
     this.socio.nombre = this.inputNombreSocio;
     this.socio.apellido = this.inputApellidoSocio;
     this.socio.telefono = this.inputTelefonoSocio;
     this.socio.estado = this.inputEstadoSocio;
-    this.sociosService.createSocio(this.socio).subscribe((res: any) => {
-      Swal.fire({
-        title: 'Socio creado con exito',
-        icon: 'success',
-        confirmButtonText: 'Ok',
-        confirmButtonColor: '#00aa00',
-      }).then((result) => {
-        window.location.reload(); // Recarga la página
+    this.sociosService.createSocio(this.socio).pipe(
+      switchMap((res: any) => {
+        let fechaIni = new Date();
+        let fechaFin = new Date();
+        fechaFin.setMonth(fechaIni.getMonth() + this.inputPlan.duracion)
+        const inscripcion = new Inscripcion(null, res.idSocio, this.inputPlan.idPlan, fechaIni, fechaFin, this.inputPlan.nombre, this.socio.dni);
+        return this.inscripcionesService.createInscripcion(inscripcion);
       })
-    },
-      (error) => {
+    ).subscribe({
+      next: (response: any) => {
+        Swal.showLoading();
         Swal.fire({
-          title: 'Error al crear el socio',
-          text: error.error,
+          title: 'Inscripción creada con éxito',
+          icon: 'success',
+          confirmButtonText: 'Ok',
+          confirmButtonColor: '#00aa00',
+        }).then(() => {
+          window.location.reload(); // Recarga la página
+        });
+      },
+      error: (error: any) => {
+        Swal.fire({
+          title: 'Error al crear la inscripción',
+          text: error.message || error,
           icon: 'error',
           confirmButtonText: 'Ok',
           confirmButtonColor: '#0000aa'
-        })
-      });
+        });
+      }
+    }
+    )
   }
+
+
+
 
 }
