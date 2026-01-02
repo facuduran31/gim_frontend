989d184a9cc014988f1def5fb3e26432fccd3623
facuduran31 <facundoduran31@gmail.com>
2026-01-01 23:05:51 -0300

    ingreso service

---
+++ src/app/services/ingreso.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'environment';
+import { Ingreso } from '../models/ingreso';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IngresoService {
+
+  constructor(private http: HttpClient) { }
+  
+  
+    urlApi = `${environment.urlApi}/ingreso`;
+  
+  
+  
+    getIngresosByIdGimnasio(idGimnasio: number) {
+      return this.http.get<Ingreso[]>(`${this.urlApi}/gimnasio/${idGimnasio}`, { withCredentials: true })
+    }
+  
+    deleteIngreso(idIngreso: number) {
+      return this.http.delete(`${this.urlApi}/${idIngreso}`, { withCredentials: true })
+    }
+  
+    createInscripcion(ingreso: Ingreso) {
+      return this.http.post(this.urlApi, ingreso, { withCredentials: true })
+    }
+}
